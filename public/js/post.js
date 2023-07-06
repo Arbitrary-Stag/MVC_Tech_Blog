e5c0fc831993f13dbf82fe72e885ac3936fe053a
Arbitrary Stag <will.weaver.iv@gmail.com>
2023-07-06 16:09:27 -0700

    Built out the handlebars file for individual blog posts, and added the post javascript file to the public folder. Also added api routes for comments.

---
+++ public/js/post.js
@@ -0,0 +1,29 @@
+const newCommentHandler = async (event) => {
+    event.preventDefault();
+  
+    const comment = document.querySelector('#comment-full').value.trim();
+    const url = window.location.href;
+    const post_id = url.substring(url.lastIndexOf('/') + 1);
+    
+    
+    if (comment) {
+      const response = await fetch(`/api/comment`, {
+        method: 'POST',
+        body: JSON.stringify({ comment, post_id }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert('Failed to create comment');
+      }
+    }
+  };
+  
+
+  document
+    .querySelector('.new-comment-form')
+    .addEventListener('submit', newCommentHandler);
