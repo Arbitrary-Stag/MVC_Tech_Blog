d4f591e3e44e080b71f23b841e3401fe91724ffe
Arbitrary Stag <will.weaver.iv@gmail.com>
2023-07-06 16:56:25 -0700

    added edit post handlebars, javascript, and routes. Also built out the html routes.

--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
+const editpostRoutes = require('./editpostRoutes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('./comment', commentRoutes);
+router.use('./editpost', editpostRoutes);
 module.exports = router;
---
+++ public/js/editpost.js
@@ -0,0 +1,40 @@
+const updateFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#new-post-title').value.trim();
+    const content = document.querySelector('#new-post-desc').value.trim();
+  
+    const url = window.location.href;
+    const post_id = url.substring(url.lastIndexOf('/') + 1);
+    
+    if (title && content) {
+      const response = await fetch(`/api/editpost/${post_id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, content }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to update post');
+      }
+    }
+};
+
+const cancelButtonHandler = (event) => {
+  console.log("cancel button clicked");
+   event.preventDefault();
+  document.location.replace('/dashboard');
+};
+
+document
+  .querySelector('.update-blogpost-form')
+  .addEventListener('submit', updateFormHandler);
+
+document
+.querySelector('#cancelBtn')
+.addEventListener('click', cancelButtonHandler);
+  
